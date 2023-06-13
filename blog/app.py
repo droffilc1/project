9c9e0d498cc306bef1f5edc72a6eda5e501101a9
Clifford Mapesa <cliffordmapesa8@gmail.com>
2023-06-13 16:23:54 +0300

    Connect to the database using SQLMode

--- blog/app.py
+++ blog/app.py
@@ -1,6 +1,17 @@
+import os
 from flask import Flask
+from db import db
+
+# Generate current directory
+base_dir = os.path.dirname(os.path.realpath(__file__))
 
 def create_app():
     app = Flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///' + \
+            os.path.join(base_dir, 'blog.db')
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["SECRET_KEY"] = 'iLhqyLO3HtSpsE8cuQaj'	
+    db.init_app(app)
+    
 
     return app
